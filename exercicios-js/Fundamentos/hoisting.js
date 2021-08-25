87f1417bf3e05c12066c77337b63f33f67cecca3
GankSir <sirgank13@gmail.com>
2021-08-25 16:46:52 -0300

    hoisting

---
+++ exercicios-js/Fundamentos/hoisting.js
@@ -0,0 +1,10 @@
+//hoisting é um efeito da linguagem
+//o Hoisting é o içamento da variavel
+// mesmo dentro da função acontece o içamento
+  console.log('a =', a)
+  var a = 2
+  console.log('a =', a)
+
+  console.log('b =', b)
+  let b = 2
+  console.log('b =', b)
--- exercicios-js/Fundamentos/usandoletloop2.js
+++ exercicios-js/Fundamentos/usandoletloop2.js
@@ -1,5 +1,4 @@
 const funcs = []
-
 for (let i = 0; i < 10; i++){
   funcs.push(function () {
     console.log(i)
