cc2b1741d8583c82c3d073895e396b09668269d7
GankSir <sirgank13@gmail.com>
2021-08-25 16:09:41 -0300

    function push

--- exercicios-js/Fundamentos/usandovarloop2.js
+++ exercicios-js/Fundamentos/usandovarloop2.js
@@ -5,6 +5,7 @@ for (var i = 0; i < 10; i++){
     console.log(i)
   })
 }
-funcs[2]()//Nesse caso o resultado é 3
-funcs[8]()//Nesse caso resultado vai ser 9
+funcs[2]()//Nesse caso o resultado é 10
+funcs[8]()//Nesse caso resultado vai ser 10
+console.log()
 //Lembrete(.push adiciona novos elementos no array)
