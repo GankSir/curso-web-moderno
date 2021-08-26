67d81067dee45dbb0c82a9832fc6db16744a788a
GankSir <sirgank13@gmail.com>
2021-08-26 15:37:21 -0300

    Operadores aritimeticos

---
+++ exercicios-js/Fundamentos/aritimeticos.js
@@ -0,0 +1,16 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 //se for par o resto da divisao é 0 se for impar da 1
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+//d++ esse é um operador chamado de postfix
+//++d esse é um operador Prefix
+//op1 + op2 sintaxe infix operador binario
+// -a Operador unario
+
+
+
