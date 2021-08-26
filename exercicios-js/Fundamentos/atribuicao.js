6d29d03e6f2205a6eb2be2fd9377792b36d0a08c
GankSir <sirgank13@gmail.com>
2021-08-26 11:01:47 -0300

    assignment

---
+++ exercicios-js/Fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 // esse simbolo de = é uma atribuição
+let b = 3
+
+b += a //isso significa que vamos fazer uma atribuição na letra A
+        // e junto com a atribuição estamos fazendo uma adição
+        // b = b + a
+console.log(b)
+b -= 4 // b = b - 4
+console.log(b)
+b *= 2//b = b * 2
+console.log(b)
+b /= 2 // b = b / 2
+console.log(b)
+b %= 2 // b = b % 2
+console.log(b)
+
+//sempre o simbolo de aritmetica vem antes da atribuição
