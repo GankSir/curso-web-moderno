d19b9c78d1a19bd38e88a5b13a6d60d5c1450568
GankSir <sirgank13@gmail.com>
2021-08-26 16:01:06 -0300

    Operadores relacionais

---
+++ exercicios-js/Fundamentos/operadoresrelacionais.js
@@ -0,0 +1,14 @@
+//Operadores relacionais sempre dara true ou false
+console.log('01', '1' == 1)
+console.log('02)', '1' === 1)
+console.log('03)', '3' != 3)
+console.log('04)', '3' !== 3)
+
+console.log('05)', 3 < 2)
+console.log('06)', 3 > 2)
+console.log('07)', 3 <= 2)
+console.log('3)', 3 >= 2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log()
