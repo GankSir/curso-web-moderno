32fd33592765501556c17035bb03676c2aa02718
GankSir <sirgank13@gmail.com>
2021-09-01 17:37:28 -0300

    this bind

---
+++ exercicios-js/controle/funcao/this.js
@@ -0,0 +1,4 @@
+//This seria uma forma de diferenciar o objeto daquela execuçao
+//quando se tem uma função declarada com function o this pode variar
+//usando uma função arrow function o this não vai variar nunca
+//tem como amarrar o this para nunca variar usando o bind
