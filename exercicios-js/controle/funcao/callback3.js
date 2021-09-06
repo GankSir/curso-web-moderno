3bdfa9fd237572105a9062bddd146755ffb2ecb5
GankSir <sirgank13@gmail.com>
2021-09-06 14:11:22 -0300

    Funcao construtora

--- exercicios-js/controle/funcao/callback3.js
+++ exercicios-js/controle/funcao/callback3.js
@@ -1,2 +1,4 @@
 //Exemplo de callback no Browser
-document.getElementsByTagName('body')[0]
+document.getElementsByTagName('body')[0].onclick = function (e) {
+  console.log('O evento ocorreu')
+}
--- exercicios-js/controle/funcao/funcoesconstrutora.js
+++ exercicios-js/controle/funcao/funcoesconstrutora.js
@@ -3,6 +3,23 @@ function carro (velocidadeMaxima = 200, delta = 5) {
   let velocidadeAtual = 0
   //metodo publico
   this.acelerar = function () {
-
+    if (velocidadeAtual + delta <= velocidadeMaxima) {
+      velocidadeAtual += delta
+    } else {
+      velocidadeAtual = velocidadeMaxima
+    }
+  }
+  //outro metodo publico criado dentro da estrutura carro
+  this.getVelocidadeAtual = function () {
+    return velocidadeAtual
   }
 }
+const uno = new carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
