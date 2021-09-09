43e45ab318dc0625c22b4660dad306b58506732e
GankSir <sirgank13@gmail.com>
2021-09-09 14:31:00 -0300

    oo

---
+++ objetos/oo.js
@@ -0,0 +1,18 @@
+//codigo não executavel
+// procedural, processamento manipulando dados
+processamento(valor1, valor2, valor3)
+
+//oo, temos objetos com atributos etem funções
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento () {
+
+  }
+}
+objeto.processamento()//O Foco passou ser o objeto
+//Podemos invocar esses processamentos apartir do dado
+// Principios importante
+//1. abstração
+//2. encapsulamento
