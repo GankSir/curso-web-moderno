244f192d1762a7f21b04bfe3b66f707a5d816949
GankSir <sirgank13@gmail.com>
2021-09-15 16:35:39 -0300

    G/S teste final

--- objetos/getteresetters.js
+++ objetos/getteresetters.js
@@ -1,5 +1,5 @@
 //o metado get é usado para ler o valor de uma variavel
-//o metodo set é usado para alterar o valor de uma variavel...!!!
+//o metodo set é usado para alterar o valor de uma variavel...!!!!!!!!
 
 const sequencia = {
     _valor: 1, //convenção
