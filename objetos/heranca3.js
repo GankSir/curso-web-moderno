05e1373173a340075aeafead35617f0633effa2a
GankSir <sirgank13@gmail.com>
2021-09-16 18:06:42 -0300

    heranca

--- objetos/heranca3.js
+++ objetos/heranca3.js
@@ -1,4 +1,21 @@
-const pai = { nome: 'Pedro', cabelo: 'Preto' }
-
+const pai = { nome: 'Pedro', corCabelo: 'Preto' }
+//pode observar que criamos um objeto filho a partir do Object.create
 const filha1 = Object.create(pai)
 filha1.nome = 'Ana'
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+    nome: { value: 'Bia', writable: false, enumerable: true }
+})
+
+console.log(filha2.nome)
+filha2.nome = 'Carla'
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for (let key in filha2) {
+    filha2.hasOwnProperty(key) ? //Aquela propriedade pertence a ela
+    console.log(key): console.log(`Por herança:${key}`)
+}
