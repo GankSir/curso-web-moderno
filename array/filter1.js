9c0aac5912c9f776d539918c77d121fbbd75691d
GankSir <sirgank13@gmail.com>
2021-10-04 14:41:42 -0300

    filter

---
+++ array/filter1.js
@@ -0,0 +1,15 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de plastico', preco: 18.99, fragil: false }
+]
+//Nesse caso o parametro callback é obrigatorio
+console.log(produtos.filter(function (p) {
+    return false
+}))
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro))
