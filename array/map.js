2847557a91748c59c429b802b77e2a8919e1c2f2
GankSir <sirgank13@gmail.com>
2021-10-04 11:37:27 -0300

    map

--- array/map.js
+++ array/map.js
@@ -2,6 +2,7 @@ const nums = [1, 2, 3, 4, 5]
 
 //for com proposito
 //o map não transforma o array atual, ele geral outro array 
+//map não altera o array original 
 let resultado = nums.map(function (e) {
     return e * 2
 })
@@ -12,4 +13,5 @@ const soma10 = e => e + 10
 const triplo = e => e * 3
 const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
 
-rrsultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(resultado)
