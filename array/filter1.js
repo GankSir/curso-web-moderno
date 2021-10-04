1c0ea232ee3f37eaeff8314ce5e9256c988c6fee
GankSir <sirgank13@gmail.com>
2021-10-04 14:56:56 -0300

    filter 2

--- array/filter1.js
+++ array/filter1.js
@@ -9,7 +9,7 @@ console.log(produtos.filter(function (p) {
     return false
 }))
 
-const caro = produto => produto.preco >= 500
+const caro = produto => produto.preco >= 2500
 const fragil = produto => produto.fragil
 
 console.log(produtos.filter(caro))
