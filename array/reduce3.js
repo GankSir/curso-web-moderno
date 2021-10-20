b1ad3fcfd69b903588fff9e57d775c532faf2e6a
GankSir <sirgank13@gmail.com>
2021-10-20 18:04:21 -0300

    reduce implementacao

---
+++ array/reduce3.js
@@ -0,0 +1,14 @@
+Array.prototype.reduce2 = function (callBack, valorInicial) {
+    const indiceInicial= valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for (let i = 1; i < this.length; i++) {
+        acumulador = callBack(acumulador, this[i], i, this)
+
+    }
+    return acumulador
+}
+
+
+const soma = (total, valor) => total + valor
+const nums = [1, 2, 3, 4, 5, 6]
+console.log(nums.reduce2(soma))
