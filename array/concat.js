1e7f9f9024647397fcffa9630b67a312c12b6fa9
GankSir <sirgank13@gmail.com>
2021-10-20 18:45:06 -0300

    concat

---
+++ array/concat.js
@@ -0,0 +1,5 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Vesclei']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
