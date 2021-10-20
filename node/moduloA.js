6ffb870113311c62088e52b7ae369636954fc3c3
GankSir <sirgank13@gmail.com>
2021-10-20 20:55:15 -0300

    node

---
+++ node/moduloA.js
@@ -0,0 +1,5 @@
+//Em node um arquivo representa um modulo 
+//modulo tem um interioridade 
+this.ola = ' fala pessoal'
+exports.bemVindo = 'Bem vindo ao node'
+module.exports.ateLogo = 'ate o proximo exemplo'
