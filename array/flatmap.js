aa3a19be55e801ddc9bc9c3d3436011b0c6ad576
GankSir <sirgank13@gmail.com>
2021-10-20 19:59:10 -0300

    mapYT

--- array/flatmap.js
+++ array/flatmap.js
@@ -20,15 +20,21 @@ const escola = [{
 const getNotaDoAluno = aluno => aluno.nota
 const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
 
-const notas1= escola.map(getNotasDaTurma)
+const notas1 = escola.map(getNotasDaTurma)
 console.log(notas1)
 
-console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ]))
+console.log([].concat([8.1, 9.3], [8.9, 7.3]))
 
-Array.prototype.flatMap = function(callBack){
+Array.prototype.flatMap = function (callBack) {
     return Array.prototype.concat.apply([], this.map(callBack))
-    
+
 }
 
 const notas2 = escola.flatMap(getNotasDaTurma)
-console.log(notas2)
+console.table(notas2)
+
+const novoArrayNivel2 = [
+    [1, 2],
+    [3, 4]
+]
+console.log(novoArrayNivel2.flat(1))
---
+++ array/mapYT.js
@@ -0,0 +1,13 @@
+//const numbers = [1, 2, 3, 4, 5]
+
+//const twice = numbers.map((batata) => batata * 2)
+
+//console.log(twice)
+
+const users = [
+    { id: 1, name: 'Bruno' },
+    { id: 2, name: 'Lucas' },
+    { id: 3, name: 'Marcos' }
+]
+
+console.log(users.map((user) => user.id))
