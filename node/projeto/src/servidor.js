f19d954f69429ae46b2e1c01e66c559d7556329b
GankSir <sirgank13@gmail.com>
2021-11-04 12:42:45 -0300

    banco de dados

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,1 +1,24 @@
 const porta = 3003
+
+const express = require('express')
+const app = express()
+const bancoDeDados = require('./bancosDeDados')
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProduto)// vai converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.sed(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('produtos', function (req, res, next) {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+})
+
+app.listen(porta, () => {
+    console.log(`servidors executando na porta ${porta},`)
+})
